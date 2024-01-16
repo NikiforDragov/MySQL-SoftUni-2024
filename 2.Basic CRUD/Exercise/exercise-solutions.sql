9a465004e5a755f58b4bdcc82477af4fe2895117
Nikifor Dragov <ndragov99@gmail.com>
2024-01-16 18:19:05 +0200

    add solution

--- 2.Basic CRUD/Exercise/exercise-solutions.sql
+++ 2.Basic CRUD/Exercise/exercise-solutions.sql
@@ -14,6 +14,9 @@ SELECT first_name,middle_name,last_name
 FROM employees
 ORDER BY employee_id;
 
+SELECT CONCAT(first_name, '.',last_name,'@','softuni.bg') AS 'full_email_address'
+FROM employees;
+
 
 
 
