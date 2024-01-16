95b2d19ad1b8325933d33a246e76e38d0ef1c6ee
Nikifor Dragov <ndragov99@gmail.com>
2024-01-16 18:13:55 +0200

    add solution

--- 2.Basic CRUD/Exercise/exercise-solutions.sql
+++ 2.Basic CRUD/Exercise/exercise-solutions.sql
@@ -5,3 +5,20 @@ ORDER BY department_id;
 SELECT `name` 
 FROM departments
 ORDER BY department_id;
+
+SELECT first_name,last_name,salary
+FROM employees
+ORDER BY employee_id;
+
+SELECT first_name,middle_name,last_name
+FROM employees
+ORDER BY employee_id;
+
+
+
+
+
+
+
+
+
