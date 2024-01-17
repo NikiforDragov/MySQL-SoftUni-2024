43138ff73ad06567ebe87c509a152ab001b41720
Nikifor Dragov <ndragov99@gmail.com>
2024-01-17 18:03:00 +0200

    Add to demo

--- 3. Built-in Functions/Lab/Lab demo code.sql
+++ 3. Built-in Functions/Lab/Lab demo code.sql
@@ -2,3 +2,9 @@ SELECT title
 FROM books
 WHERE SUBSTRING(title, 1, 4) = 'The '
 ORDER BY id;
+
+SELECT 
+REPLACE(title, 'The', '***') AS title
+FROM books
+WHERE SUBSTRING(title, 1, 4) = 'The '
+ORDER BY id;
