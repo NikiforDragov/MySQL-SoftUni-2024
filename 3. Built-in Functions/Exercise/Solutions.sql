7b00d32cb15c7ab331a17b94ff31593e980feceb
Nikifor Dragov <ndragov99@gmail.com>
2024-01-20 22:12:30 +0200

    Add solution

--- 3. Built-in Functions/Exercise/Solutions.sql
+++ 3. Built-in Functions/Exercise/Solutions.sql
@@ -98,6 +98,29 @@ FROM users
 WHERE ip_address LIKE "___.1%.%.___"
 ORDER BY user_name;
 
+-- Exercise 15
+SELECT 
+`name`,
+CASE 
+WHEN EXTRACT(HOUR FROM `start`) BETWEEN 0 AND 11
+THEN 'Morning'
+WHEN EXTRACT(HOUR FROM `start`) BETWEEN 12 AND 17
+THEN 'Afternoon'
+WHEN EXTRACT(HOUR FROM `start`) BETWEEN 18 AND 23
+THEN 'Evening'
+END AS 'Part of the Day',
+CASE
+WHEN duration <= 3
+THEN 'Extra Short'
+WHEN duration BETWEEN 4 AND 6
+THEN 'Short'
+WHEN duration BETWEEN 7 AND 10
+THEN 'Long'
+ELSE 'Extra Long'
+END AS Duration
+FROM games;
+
+
 
 
 
