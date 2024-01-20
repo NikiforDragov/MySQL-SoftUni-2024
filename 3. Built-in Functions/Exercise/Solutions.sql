4a3482ecfc7e218b9cdd78731d3b5abb31da7900
Nikifor Dragov <ndragov99@gmail.com>
2024-01-20 20:57:34 +0200

    Add solution

--- 3. Built-in Functions/Exercise/Solutions.sql
+++ 3. Built-in Functions/Exercise/Solutions.sql
@@ -41,6 +41,13 @@ OR `name` LIKE 'b%'
 OR `name` LIKE 'e%'
 ORDER BY `name`; 
 
+-- Exercise 7
+SELECT * 
+FROM towns
+WHERE `name` NOT LIKE 'r%' 
+AND `name` NOT LIKE 'b%'
+AND `name` NOT LIKE 'd%'
+ORDER BY `name`; 
 
 
 
