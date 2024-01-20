46af9d61e1dceb064cf16731102dc8f47a983690
Nikifor Dragov <ndragov99@gmail.com>
2024-01-20 21:43:49 +0200

    Add solution

--- 3. Built-in Functions/Exercise/Solutions.sql
+++ 3. Built-in Functions/Exercise/Solutions.sql
@@ -85,6 +85,12 @@ WHERE EXTRACT(YEAR FROM `start`) BETWEEN 2011 AND 2012
 ORDER BY `start`, `name`
 LIMIT 50;
 
+-- Exercise 13
+SELECT 
+user_name,
+SUBSTR(email, (LOCATE('@', email) + 1)) AS 'email provider'
+FROM users
+ORDER BY `email provider`, user_name 
 
 
 
