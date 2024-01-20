4c38649ff5b405973be6c774216784b31fc98945
Nikifor Dragov <ndragov99@gmail.com>
2024-01-20 20:01:39 +0200

    add solution

--- 3. Built-in Functions/Exercise/Solutions.sql
+++ 3. Built-in Functions/Exercise/Solutions.sql
@@ -3,3 +3,30 @@ SELECT first_name, last_name
 FROM  employees
 WHERE first_name LIKE 'Sa%'
 ORDER BY employee_id;
+
+-- Exercise 2
+SELECT first_name, last_name
+FROM  employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
