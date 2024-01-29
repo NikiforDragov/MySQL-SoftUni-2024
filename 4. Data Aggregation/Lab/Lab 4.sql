fc6cb5854dd808605dbda91c93618da47c0e0b1e
Nikifor Dragov <ndragov99@gmail.com>
2024-01-29 12:16:45 +0200

    Add Solution

--- 4. Data Aggregation/Lab/Lab 4.sql
+++ 4. Data Aggregation/Lab/Lab 4.sql
@@ -29,6 +29,15 @@ ORDER BY department_id, `Number of employees`;
   FROM products
   WHERE category_id = 2 AND price > 8;
   
+  -- Exercise 5
+  SELECT 
+  category_id,
+  ROUND(AVG(price), 2) AS 'Average Price',
+  ROUND(MIN(price), 2) AS 'Cheapest Product',
+  ROUND(MAX(price), 2) AS 'Most Expensive Product'
+  FROM products
+  GROUP BY category_id;
+  
   
   
   
