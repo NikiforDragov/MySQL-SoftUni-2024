722cce9db82e854435594ed65433b1ea70a8aa10
Nikifor Dragov <ndragov99@gmail.com>
2024-01-29 12:05:09 +0200

    Add solution

--- 4. Data Aggregation/Lab/Lab 4.sql
+++ 4. Data Aggregation/Lab/Lab 4.sql
@@ -1,7 +1,16 @@
 SELECT * FROM employees;
 
+-- Exercise 1 
 SELECT department_id,
 COUNT(department_id) AS 'Number of employees'
 FROM employees
 GROUP BY department_id
 ORDER BY department_id, `Number of employees`;
+
+-- Exercise 2
+  SELECT 
+  department_id,
+  ROUND(AVG(salary), 2) AS 'Average Salary'
+  FROM employees
+  GROUP BY department_id
+  ORDER BY department_id;
