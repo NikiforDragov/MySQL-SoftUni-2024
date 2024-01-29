016dc1dda2684e15062144250373bf2c7d753351
Nikifor Dragov <ndragov99@gmail.com>
2024-01-29 12:08:26 +0200

    Add Solution

--- 4. Data Aggregation/Lab/Lab 4.sql
+++ 4. Data Aggregation/Lab/Lab 4.sql
@@ -14,3 +14,16 @@ ORDER BY department_id, `Number of employees`;
   FROM employees
   GROUP BY department_id
   ORDER BY department_id;
+  
+  -- Exercise 3
+  SELECT 
+  department_id,
+  ROUND(MIN(salary), 2) AS 'Min Salary'
+  FROM employees
+  GROUP BY department_id
+  HAVING `Min Salary` > 800;
+  
+  
+  
+  
+  
