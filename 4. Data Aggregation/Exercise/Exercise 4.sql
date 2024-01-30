2696d6e59d0a05f64263b69ecaceaff65b1788fc
Nikifor Dragov <ndragov99@gmail.com>
2024-01-30 14:19:51 +0200

    Add solution

--- 4. Data Aggregation/Exercise/Exercise 4.sql
+++ 4. Data Aggregation/Exercise/Exercise 4.sql
@@ -110,4 +110,18 @@ FROM wizzard_deposits;
  HAVING department_id IN(2,5,7)
  ORDER BY department_id;
  
+ -- Exercise 13 
+ 
+ -- Exercise 14
+ SELECT 
+ department_id,
+ MAX(salary) AS `max_salary`
+ FROM employees
+ GROUP BY department_id
+ HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+ ORDER BY department_id;
+ 
+ 
+ 
+ 
  
