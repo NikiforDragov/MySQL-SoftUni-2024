d60130d25e702f0da61275f69994b250dabf7fa3
Nikifor Dragov <ndragov99@gmail.com>
2024-01-30 12:33:40 +0200

    Add solution

--- 4. Data Aggregation/Exercise/Exercise 4.sql
+++ 4. Data Aggregation/Exercise/Exercise 4.sql
@@ -9,3 +9,13 @@ FROM wizzard_deposits;
  SELECT 
  MAX(magic_wand_size) AS 'longest_magic_wand'
  FROM wizzard_deposits;
+ 
+ -- Exercise 3
+ SELECT 
+ deposit_group,
+ MAX(magic_wand_size) AS 'longest_magic_wand'
+ FROM wizzard_deposits
+ GROUP BY deposit_group
+ ORDER BY `longest_magic_wand`, deposit_group;
+ 
+ 
