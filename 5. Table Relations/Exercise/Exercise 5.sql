62259650833cf537df404a1a9e03962e9726cfce
Nikifor Dragov <ndragov99@gmail.com>
2024-02-02 18:10:53 +0200

    Add Solution

--- 5. Table Relations/Exercise/Exercise 5.sql
+++ 5. Table Relations/Exercise/Exercise 5.sql
@@ -164,6 +164,41 @@ CREATE TABLE order_items(
     FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
 
+-- Exercise 6
+ CREATE TABLE subjects(
+	subject_id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_name VARCHAR(50)
+ );
+ 
+ CREATE TABLE majors(
+	major_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+ );
+ 
+ CREATE TABLE students(
+	student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_number VARCHAR(12),
+    student_name VARCHAR(50),
+    major_id INT,
+    FOREIGN KEY (major_id) REFERENCES majors(major_id)
+ );
+ 
+ CREATE TABLE payments(
+	payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    payment_date DATE,
+    payment_amount DECIMAL(8,2),
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+ );
+ 
+ CREATE TABLE  agenda(
+	student_id INT,
+    subject_id INT,
+    PRIMARY KEY (student_id, subject_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+ );
+ 
 
 
 
