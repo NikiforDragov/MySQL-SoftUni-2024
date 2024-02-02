372332767ad6ab38b5565cc7d6ca71aa9ea3f31d
Nikifor Dragov <ndragov99@gmail.com>
2024-02-02 13:37:37 +0200

    Add solution

--- 5. Table Relations/Exercise/Exercise 5.sql
+++ 5. Table Relations/Exercise/Exercise 5.sql
@@ -26,3 +26,43 @@ VALUES
 (2, 'Tom', 56100, 103),
 (3, 'Yana', 60200, 101);
 
+-- Exercise 2
+CREATE TABLE manufacturers(
+	manufacturer_id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL UNIQUE,
+    established_on DATE
+);
+
+INSERT INTO manufacturers (`name`, established_on)
+VALUES
+('BMW', '1916-03-01'),
+('Tesla', '2003-01-01'),
+('Lada', '1966-05-01');
+
+CREATE TABLE models(
+	model_id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL UNIQUE,
+    manufacturer_id INT,
+    CONSTRAINT fk_models_manufacturer
+    FOREIGN KEY (manufacturer_id)
+    REFERENCES manufacturers(manufacturer_id)
+);
+
+INSERT INTO models(model_id, `name`, manufacturer_id)
+VALUES
+('101', 'X1', 1),
+('102', 'i6', 1),
+('103', 'Model S', 2),
+('104', 'Model X', 2),
+('105', 'Model 3', 2),
+('106', 'Nova', 3);
+
+
+
+
+
+
+
+
+
+  
