8fa510101a73735129992419042564aea0a15603
Nikifor Dragov <ndragov99@gmail.com>
2024-02-02 14:07:31 +0200

    Add solution

--- 5. Table Relations/Exercise/Exercise 5.sql
+++ 5. Table Relations/Exercise/Exercise 5.sql
@@ -57,9 +57,50 @@ VALUES
 ('105', 'Model 3', 2),
 ('106', 'Nova', 3);
 
+-- Exercise 3
+ CREATE TABLE students(
+	student_id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL
+ );
+ 
+ INSERT INTO students(`name`)
+ VALUES
+ ('Mila'),
+ ('Toni'),
+ ('Ron');
+ 
+ CREATE TABLE exams(
+	exam_id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL
+ );
+ 
+INSERT INTO exams(exam_id, `name`)
+VALUES
+(101, 'Spring MVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g');
 
+CREATE TABLE students_exams(
+	student_id INT,
+    exam_id INT,
+    CONSTRAINT pk_students_exams
+    PRIMARY KEY (student_id, exam_id),
+    CONSTRAINT fk_students_exams_students
+    FOREIGN KEY (student_id)
+    REFERENCES students(student_id),
+    CONSTRAINT fk_students_exams_exams
+    FOREIGN KEY (exam_id)
+    REFERENCES exams(exam_id)
+);
 
-
+INSERT INTO students_exams(student_id, exam_id)
+VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103);
 
 
 
