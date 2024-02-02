1555031c0342c22a38700f852e8648815cc94bb1
Nikifor Dragov <ndragov99@gmail.com>
2024-02-02 17:17:33 +0200

    Add solution

--- 5. Table Relations/Exercise/Exercise 5.sql
+++ 5. Table Relations/Exercise/Exercise 5.sql
@@ -102,7 +102,27 @@ VALUES
 (2,102),
 (2,103);
 
+-- Exercise 4
+CREATE TABLE teachers(
+	teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL,
+    manager_id INT
+);
 
+INSERT INTO teachers (teacher_id, `name`, manager_id)
+VALUES 
+(101, 'John', NULL),
+(102, 'Maya', 106), 
+(103, 'Silvia', 106), 
+(104, 'Ted', 105), 
+(105, 'Mark', 101), 
+(106, 'Greta', 101);
+
+ALTER TABLE teachers
+ADD 
+CONSTRAINT fk_teacher_manager
+FOREIGN KEY (manager_id)
+REFERENCES teachers(teacher_id);
 
 
 
