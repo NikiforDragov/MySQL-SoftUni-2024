5e8aa489e0ccbe7f6f48a1e57b08b6c3e83694f1
Nikifor Dragov <ndragov99@gmail.com>
2024-02-02 18:01:25 +0200

    Add solution

--- 5. Table Relations/Exercise/Exercise 5.sql
+++ 5. Table Relations/Exercise/Exercise 5.sql
@@ -124,6 +124,48 @@ CONSTRAINT fk_teacher_manager
 FOREIGN KEY (manager_id)
 REFERENCES teachers(teacher_id);
 
+-- Exercise 5
+CREATE TABLE cities(
+	city_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE customers(
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT,
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+
+CREATE TABLE orders(
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE item_types(
+	item_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE items(
+	item_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    item_type_id INT,
+    FOREIGN KEY (item_type_id) REFERENCES item_types(item_type_id)
+);
+
+CREATE TABLE order_items(
+	order_id INT,
+    item_id INT,
+    PRIMARY KEY(order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+
+
+
 
 
   
