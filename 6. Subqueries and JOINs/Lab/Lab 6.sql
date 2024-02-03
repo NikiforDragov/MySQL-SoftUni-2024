d2caf052897c99db09a59367b8ef3f741e6cfd76
Nikifor Dragov <ndragov99@gmail.com>
2024-02-03 19:11:35 +0200

    Add

--- 6. Subqueries and JOINs/Lab/Lab 6.sql
+++ 6. Subqueries and JOINs/Lab/Lab 6.sql
@@ -9,6 +9,17 @@ FROM departments
 ORDER BY employee_id
 LIMIT 5;
 
+-- 2
+SELECT 
+	a.town_id,
+    t.name,
+    a.address_text
+FROM addresses AS a
+	JOIN towns AS t ON a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY a.town_id, a.address_id;
+
+
 -- 4
 SELECT 
     COUNT(*) AS 'count'
