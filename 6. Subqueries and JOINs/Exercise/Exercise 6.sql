70913443b7b8346686fbba122be5ac55aaa683cc
Nikifor Dragov <ndragov99@gmail.com>
2024-02-04 13:48:48 +0200

    Add

--- 6. Subqueries and JOINs/Exercise/Exercise 6.sql
+++ 6. Subqueries and JOINs/Exercise/Exercise 6.sql
@@ -111,4 +111,20 @@ WHERE
     e.manager_id IN (3 , 7)
 ORDER BY e.first_name;
 
+-- Exercise 10 
+SELECT 
+    e.employee_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS 'employee_name',
+    CONCAT(m.first_name, ' ', m.last_name) AS 'manager_name',
+    d.name AS 'department_name'
+FROM
+    employees e
+        JOIN
+    employees m ON m.employee_id = e.manager_id
+		JOIN
+	departments d ON e.department_id = d.department_id
+WHERE
+    e.manager_id IS NOT NULL
+ORDER BY e.employee_id
+LIMIT 5;
 
