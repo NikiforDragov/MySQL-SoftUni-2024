84e6927a3a5c2039590c941fa612b9372afc0706
Nikifor Dragov <ndragov99@gmail.com>
2024-02-04 17:43:59 +0200

    Add

--- 6. Subqueries and JOINs/Exercise/Exercise 6.sql
+++ 6. Subqueries and JOINs/Exercise/Exercise 6.sql
@@ -173,7 +173,23 @@ LIMIT 1;
  LIMIT 5;
   
 -- Exercise 15*
- 
+ SELECT
+	c.continent_code,
+    c.currency_code,
+    COUNT(*) AS currency_usage
+FROM
+	countries c
+GROUP BY c.continent_code, c.currency_code
+HAVING currency_usage > 1 AND currency_usage = (SELECT
+		COUNT(*) AS max_usage
+	FROM 
+		countries 
+	WHERE 
+		continent_code = c.continent_code
+	GROUP BY currency_code
+    ORDER BY max_usage DESC
+    LIMIT 1)
+ORDER BY c.continent_code, c.currency_code;
  
 -- Exercise 16
 SELECT COUNT(c.country_code) AS 'country_count'
