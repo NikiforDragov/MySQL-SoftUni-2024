64adc4a78834f142226ec6e08b8007c5f190700b
Nikifor Dragov <ndragov99@gmail.com>
2024-02-04 12:13:49 +0200

    Add

---
+++ 6. Subqueries and JOINs/Exercise/Exercise 6.sql
@@ -0,0 +1,10 @@
+-- Exercise 1
+SELECT 
+	e.employee_id,
+    e.job_title,
+    e.address_id,
+    a.address_text
+FROM employees AS e
+	JOIN addresses AS a ON a.address_id =  e.address_id
+ORDER BY e.address_id ASC
+LIMIT 5;
