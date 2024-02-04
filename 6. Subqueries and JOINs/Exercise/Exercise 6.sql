57640edf205b3f00344778ec39db2f9f1f9cf752
Nikifor Dragov <ndragov99@gmail.com>
2024-02-04 13:40:26 +0200

    Add

--- 6. Subqueries and JOINs/Exercise/Exercise 6.sql
+++ 6. Subqueries and JOINs/Exercise/Exercise 6.sql
@@ -97,6 +97,18 @@ WHERE
     e.employee_id = 24
 ORDER BY project_name;
 
-
+-- Exercise 9
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name AS 'manager_name'
+FROM
+    employees e
+        JOIN
+    employees m ON m.employee_id = e.manager_id
+WHERE
+    e.manager_id IN (3 , 7)
+ORDER BY e.first_name;
 
 
