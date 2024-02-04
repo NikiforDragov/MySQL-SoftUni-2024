ba046fb3d4f07fffabe09c3d3fa314b137174e4b
Nikifor Dragov <ndragov99@gmail.com>
2024-02-04 12:18:57 +0200

    Add

--- 6. Subqueries and JOINs/Exercise/Exercise 6.sql
+++ 6. Subqueries and JOINs/Exercise/Exercise 6.sql
@@ -8,3 +8,15 @@ FROM employees AS e
 	JOIN addresses AS a ON a.address_id =  e.address_id
 ORDER BY e.address_id ASC
 LIMIT 5;
+
+-- Exercise 2
+ SELECT 
+	first_name,
+    last_name,
+    t.name,
+    a.address_text
+FROM employees AS e
+	JOIN addresses AS a ON a.address_id =  e.address_id
+    JOIN towns AS t ON a.town_id = t.town_id
+ORDER BY first_name ASC, last_name
+LIMIT 5;
