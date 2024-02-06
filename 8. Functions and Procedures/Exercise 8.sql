a2d033fd937d1985f9edefed6e84bef15d93eeec
Nikifor Dragov <ndragov99@gmail.com>
2024-02-06 18:39:31 +0200

    Add

--- 8. Functions and Procedures/Exercise 8.sql
+++ 8. Functions and Procedures/Exercise 8.sql
@@ -42,6 +42,23 @@ CALL usp_get_towns_starting_with('be') $
 DROP PROCEDURE usp_get_towns_starting_with $
 
 
+ -- Exercise 4
+ CREATE PROCEDURE usp_get_employees_from_town(town VARCHAR(50))
+ BEGIN
+	SELECT 
+    e.first_name,
+    e.last_name
+    FROM employees e
+    JOIN addresses a ON e.address_id = a.address_id
+    JOIN towns t ON t.town_id = a.town_id
+    WHERE t.name = town
+    ORDER BY e.first_name, e.last_name, e.employee_id;
+ END $
+ 
+ CALL usp_get_employees_from_town('Sofia') $
+ 
+	
+
 
 
 
