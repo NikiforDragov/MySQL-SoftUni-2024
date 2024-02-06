f8a5c7efe56c8646fdc484c2f04dd2232937cdee
Nikifor Dragov <ndragov99@gmail.com>
2024-02-06 17:28:17 +0200

    Add

--- 7. Programmability and Transactions/Lab 8.sql
+++ 7. Programmability and Transactions/Lab 8.sql
@@ -12,11 +12,37 @@ BEGIN
     RETURN e_count;
 END $
 
-DELIMITER ;
-
 SELECT ufn_count_employees_by_town('Plovdiv')
 
+-- Exercise 2
+
+DELIMITER $
+
+CREATE PROCEDURE  usp_raise_salaries(department_name VARCHAR(50))
+BEGIN
+	UPDATE employees AS e
+		JOIN departments AS d
+	ON e.department_id = d.department_id
+    SET salary = salary * 1.05
+    WHERE d.name = department_name;
+END $
+
+DELIMITER ; 
 
 
+-- Exercise 3
+DELIMITER $
+
+
+
+
+
+DELIMITER ;
+
+
+
+
+
+ 
 
 
