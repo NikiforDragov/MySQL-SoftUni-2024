4c6c625304f5153683fed405b427de8416cca883
Nikifor Dragov <ndragov99@gmail.com>
2024-02-06 18:04:48 +0200

    Add

--- 8. Functions and Procedures/Exercise 8.sql
+++ 8. Functions and Procedures/Exercise 8.sql
@@ -13,6 +13,19 @@ END $
 
 CALL usp_get_employees_salary_above_35000 $ 
 
+-- Exercise 2
+ 
+ CREATE PROCEDURE usp_get_employees_salary_above(above DECIMAL(16,4))
+BEGIN
+	SELECT 
+		first_name,
+		last_name
+    FROM employees
+    WHERE salary >= above
+    ORDER BY first_name, last_name, employee_id;
+END $
+
+CALL usp_get_employees_salary_above(1500.0000) $
 
 
 
