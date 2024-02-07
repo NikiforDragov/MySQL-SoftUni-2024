5148c365e923251bb7f73cd03ccca37cba1a084e
Nikifor Dragov <ndragov99@gmail.com>
2024-02-07 20:31:31 +0200

    Add

--- 8. Functions and Procedures/Exercise 8.sql
+++ 8. Functions and Procedures/Exercise 8.sql
@@ -122,8 +122,26 @@ BEGIN
 	GROUP BY ah.id
 	ORDER BY ah.id;
 END $
+
 -- Exercise 10
-  
+CREATE FUNCTION ufn_calculate_future_value(initial DECIMAL(19,4), interest_rate DOUBLE, numYears INT)
+RETURNS DECIMAL(10,4)
+READS SQL DATA
+BEGIN
+	RETURN initial * POW(1 + interest_rate, numYears);
+END $
+
+-- Exercise 11
+CREATE PROCEDURE usp_calculate_future_value_for_account(acc_id INT, interest DECIMAL(10,4))
+BEGIN
+SELECT a.id AS account_id,
+ah.first_name, ah.last_name,
+a.balance AS current_balance,
+ufn_calculate_future_value(a.balance, interest, 5) AS balance_in_5_years
+FROM account_holders ah
+JOIN accounts a ON ah.id = a.account_holder_id
+WHERE a.id = acc_id;
+END $
 
 
 
