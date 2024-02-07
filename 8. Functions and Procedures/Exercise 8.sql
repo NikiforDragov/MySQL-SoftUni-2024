035289b21afef9b595d54b098bc7e45cba157a83
Nikifor Dragov <ndragov99@gmail.com>
2024-02-07 19:10:47 +0200

    Add

--- 8. Functions and Procedures/Exercise 8.sql
+++ 8. Functions and Procedures/Exercise 8.sql
@@ -111,8 +111,17 @@ END $
 CALL usp_get_holders_full_name $
 
 -- Exercise 9
- 
-
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(target_salary DECIMAL(19,4))
+BEGIN
+	SELECT ah.first_name, ah.last_name FROM account_holders ah
+    JOIN accounts a ON ah.id = a.account_holder_id
+    WHERE target_salary < ( SELECT SUM(balance)
+							FROM accounts
+                            WHERE account_holder_id = ah.id
+                            GROUP BY account_holder_id)
+	GROUP BY ah.id
+	ORDER BY ah.id;
+END $
 -- Exercise 10
   
 
