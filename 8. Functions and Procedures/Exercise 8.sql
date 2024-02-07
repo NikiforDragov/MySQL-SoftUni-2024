1cdf46873da84439afacd336766d885ca6f5f626
Nikifor Dragov <ndragov99@gmail.com>
2024-02-07 20:43:21 +0200

    Add

--- 8. Functions and Procedures/Exercise 8.sql
+++ 8. Functions and Procedures/Exercise 8.sql
@@ -143,6 +143,22 @@ JOIN accounts a ON ah.id = a.account_holder_id
 WHERE a.id = acc_id;
 END $
 
+-- Exercise 12 
+CREATE PROCEDURE usp_deposit_money(acc_id INT, money_amount DECIMAL(10,4))
+BEGIN
+	START TRANSACTION;
+    IF ((SELECT COUNT(*) FROM accounts WHERE id = acc_id) <> 1 OR money_amount < 0 ) THEN
+		ROLLBACK;
+	ELSE 
+		UPDATE accounts SET balance = balance + money_amount WHERE id = acc_id;
+        COMMIT;
+	END IF;
+END $
+	
+
+
+
+
 
 
 
