abed5ee140f56407a417679d52058c45c9ba03fc
Nikifor Dragov <ndragov99@gmail.com>
2024-02-08 16:30:26 +0200

    Add

--- Exam Preparation - 1/Exam prep 1.sql
+++ Exam Preparation - 1/Exam prep 1.sql
@@ -109,6 +109,19 @@ LEFT JOIN students_courses sc ON s.id = sc.student_id
 WHERE sc.student_id IS NULL
 ORDER BY `password` DESC; 
 
+-- -------
+
+SELECT 
+COUNT(c.id) AS students_count,
+u.name AS university_name
+FROM universities u
+JOIN courses c ON u.id = c.university_id
+JOIN students_courses sc ON c.id = sc.course_id
+GROUP BY u.name
+HAVING students_count >= 8
+ORDER BY students_count DESC, university_name DESC;
+
+
 
 
 
