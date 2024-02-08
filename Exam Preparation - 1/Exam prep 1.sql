b05465d55b2bfdb168c7db83afed67359e69888e
Nikifor Dragov <ndragov99@gmail.com>
2024-02-08 15:43:01 +0200

    Add

--- Exam Preparation - 1/Exam prep 1.sql
+++ Exam Preparation - 1/Exam prep 1.sql
@@ -56,4 +56,17 @@ CREATE TABLE students_courses(
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
+-- ---------
+
+INSERT INTO courses (name, duration_hours, start_date, teacher_name, description, university_id)
+SELECT
+	CONCAT(c.teacher_name, ' ', 'course'), 
+    LENGTH(c.name) / 10,
+    DATE(c.start_date + 5),
+    REVERSE(c.teacher_name),
+    CONCAT('Course ', c.teacher_name, REVERSE(c.description)),
+    DAY(c.start_date)
+FROM courses c
+WHERE c.id <= 5;  
+
 
