c72452c5af4a4ae56f0f8d760c7708459f388263
Nikifor Dragov <ndragov99@gmail.com>
2024-02-08 17:01:36 +0200

    Add

--- Exam Preparation - 1/Exam prep 1.sql
+++ Exam Preparation - 1/Exam prep 1.sql
@@ -143,8 +143,41 @@ FROM universities u
 JOIN cities c ON c.id = u.city_id
 ORDER BY u.tuition_fee;
 
+-- ----------
+
+DELIMITER $
+
+CREATE FUNCTION udf_average_alumni_grade_by_course_name(course_name VARCHAR(60))
+RETURNS DECIMAL(19,2)
+DETERMINISTIC
+BEGIN
+	DECLARE average_grade DECIMAL(19,2);
+    SET average_grade := (
+		SELECT
+			AVG(sc.grade) AS average_alumni_grade
+			FROM students s
+					JOIN students_courses sc ON s.id = sc.student_id
+					JOIN courses c ON c.id = sc.course_id
+					WHERE c.name = course_name AND s.is_graduated = 1);
+	RETURN average_grade;
+END $
+
+DELIMITER ;
+
+DROP FUNCTION udf_average_alumni_grade_by_course_name;
+
+SELECT c.name, udf_average_alumni_grade_by_course_name('Quantum Physics') as average_alumni_grade FROM courses c 
+WHERE c.name = 'Quantum Physics';
 
 
+
+SELECT
+ c.name AS course_name,
+ AVG(sc.grade) AS average_alumni_grade
+FROM students s
+	JOIN students_courses sc ON s.id = sc.student_id
+    JOIN courses c ON c.id = sc.course_id
+WHERE c.name = 'Quantum Physics' AND s.is_graduated = 1;
 
 
 
